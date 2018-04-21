c46565a7034da0b66121002dfdca03e5f143d2eb
Wil Taylor <wilfridtaylor@gmail.com>
2018-04-21 12:09:14 +1000

    Added bullet hell player controller

---
+++ Assets/PlayerStats.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerStats : MonoBehaviour
+{
+    public float Mana;
+    public float MaxMana;
+    public float ManaChargeRate;
+
+    public float HP;
+    public float MaxHP;
+    public float HPChargeRate;
+
+    public UnityEvent OnDeath;
+
+    private bool hasProcessedDeath = false;
+
+    public void UpdateHP(float ammount)
+    {
+        HP += ammount;
+    }
+
+    public void UpdateMana(float ammount)
+    {
+        Mana += ammount;
+    }
+
+    void Update()
+    {
+        if (hasProcessedDeath)
+            return;
+
+        if (HP <= 0f)
+        {
+            OnDeath.Invoke();
+            hasProcessedDeath = true;
+            HP = 0;
+            return;
+        }
+
+        Mana += ManaChargeRate * Time.deltaTime;
+        HP += HPChargeRate * Time.deltaTime;
+
+        if (HP > MaxHP)
+            HP = MaxHP;
+
+        if (Mana > MaxMana)
+            Mana = MaxMana;
+
+    }
+
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float Speed;
+    public float RotationSpeed;
+        
+    private Rigidbody _rigidbody;
+
+	void Start ()
+	{
+	    _rigidbody = GetComponent<Rigidbody>();
+
+	}
+	
+	void Update ()
+    {
+        if (Input.GetAxis("Horizontal") > 0.01f)
+        {
+            transform.Rotate(Vector3.up, RotationSpeed * Time.deltaTime);
+
+        }
+
+        if (Input.GetAxis("Horizontal") < -0.01f)
+        {
+            transform.Rotate(Vector3.up, -RotationSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetAxis("Vertical") > 0.01f)
+        {
+            transform.position = transform.position + transform.forward * (Speed * Time.deltaTime);
+
+        }
+
+        if (Input.GetAxis("Vertical") < -0.01f)
+        {
+            transform.position = transform.position + -transform.forward * (Speed * Time.deltaTime);
+
+        }
+
+        if (Input.GetAxis("SideStep") > 0.01f)
+        {
+            transform.position = transform.position + transform.right * (Speed * Time.deltaTime);
+
+        }
+
+        if (Input.GetAxis("SideStep") < -0.01f)
+        {
+            transform.position = transform.position + -transform.right * (Speed * Time.deltaTime);
+        }
+    }
+}
