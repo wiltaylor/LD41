a728c9ecd6e58c00e2d51a501c65b8bfa8e246d0
Wil Taylor <wilfridtaylor@gmail.com>
2018-04-22 11:59:35 +1000

    Added battle junk deployment

---
+++ Assets/BattleFieldJunkSpawner.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class BattleFieldJunkSpawner : MonoBehaviour
+{
+    public BattleFieldJunkItem[] Junk;
+    public float MinX;
+    public float MaxX;
+    public float MinZ;
+    public float MaxZ;
+    public float Y;
+    public LayerMask CheckMask;
+
+    void Start()
+    {
+        var infloopcheck = 0;
+
+        foreach (var item in Junk)
+        {
+            var qty = Random.Range(item.Min, item.Max);
+
+            for (int i = 0; i < qty; i++)
+            {
+                float x;
+                float z;
+
+                infloopcheck = 0;
+
+                while (true)
+                {
+                    infloopcheck++;
+                    x = Random.Range(MinX, MaxX);
+                    z = Random.Range(MinZ, MaxZ);
+
+                    if (!Physics.CheckSphere(new Vector3(x, Y, z), item.CheckRadius, CheckMask))
+                        break;
+
+                    if(infloopcheck > 10000)
+                        throw new ApplicationException("Infinite loop protection triggered");
+                }
+
+                var junk = Instantiate(item.Prefab);
+                junk.transform.position = new Vector3(x, Y, z);
+                junk.SetActive(true);
+            }
+        }
+    }
+}
